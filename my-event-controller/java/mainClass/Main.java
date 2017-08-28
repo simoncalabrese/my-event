63c26229e3829c53360fa0e3519099e590498d95
simon.calabrese <bartolomeomio8>
2017-08-28 12:34:23 +0200

    now run

---
+++ my-event-controller/java/mainClass/Main.java
@@ -0,0 +1,15 @@
+package mainClass;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by simon.calabrese on 28/08/2017.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] rgs) {
+        SpringApplication.run(Main.class, rgs);
+    }
+}
